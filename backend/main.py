b176b8fe75e7cce8a5b4dafe10c8e8345c37d31c
Bikash Kumar <aayushladdha001@gmail.com>
2026-04-15 23:47:41 +0530

    .

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ app = FastAPI(title="Codebase Exploration Agent")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins= ["http://localhost:5173", "https://127.0.0.1:5173"],
+    allow_origins= ["http://localhost:5173", "https://127.0.0.1:5173", "https://codebase-explainer-eex8.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -20,6 +20,10 @@ app.include_router(agent.router, prefix=BASE)
 app.include_router(chat.router, prefix=BASE)
 
 
-@app.get("/")
+# @app.get("/")
+# def root():
+#     return {"message": "Backend running 🚀"}
+
+@app.api_route("/", methods=["GET", "HEAD"])
 def root():
     return {"message": "Backend running 🚀"}
